100c7777439705863da379f8da43ae46b8299f41
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-09-04 19:38:24 +0300

    FINISHES LAB

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(name, age, sex, color,temperament, alive) VALUES
+('Mr. Chocolate', 30 , 'M','black','Protective', 1 ),
+('Rowdy', 25, 'M', 'brown', 'Affectionate', 1),
+('Tabitha', 45, 'F', 'white', 'strict', 1),
+('Sergeant Brown', 65, 'M', 'brown', 'altruistic', 0),
+('Melissa', 72, 'F', 'brown', 'playful', 0),
+('Grinch', 42 , 'M', 'black', 'fearful', 1),
+('Wendy', 35, 'F', 'black', 'social', 1 ),
+(NULL , NULL, 'M', 'red', 'attentive', 1);
